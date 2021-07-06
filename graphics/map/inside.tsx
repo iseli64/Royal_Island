5312bdedb641cb9fcb2501ee574908853992ea10
Isabelle Seliger <isabelle.seliger@gmail.com>
2021-07-06 19:39:25 -0400

    Added missing tsx file

---
+++ graphics/map/inside.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="inside" tilewidth="1672" tileheight="973" tilecount="7" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="809" height="599" source="../tiana_house/tiana_house.png"/>
+ </tile>
+ <tile id="1">
+  <image width="847" height="627" source="../pirate_house/pirate_house.png"/>
+ </tile>
+ <tile id="2">
+  <image width="810" height="600" source="../ariels_house/ariel_bedroom.png"/>
+ </tile>
+ <tile id="3">
+  <image width="809" height="599" source="../aladdin_house/aladdin_house.png"/>
+ </tile>
+ <tile id="4">
+  <image width="800" height="600" source="buildings/restaurant.png"/>
+ </tile>
+ <tile id="5">
+  <image width="1672" height="973" source="buildings/visitor_room.png"/>
+ </tile>
+ <tile id="6">
+  <image width="800" height="600" source="buildings/pirate_ship_inside.png"/>
+ </tile>
+</tileset>
